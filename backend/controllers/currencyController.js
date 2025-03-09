889e44ee984b12b3709f2b85688f8b5680dde988
Praboth Sandaruwan <bleezbub12@gmail.com>
2025-03-09 12:17:39 +0530

    currency ex is working

--- backend/controllers/currencyController.js
+++ backend/controllers/currencyController.js
@@ -3,10 +3,12 @@ const axios = require("axios");
 exports.getExchangeRate = async (req, res) => {
   try {
     const { base, target } = req.query;
-    const response = await axios.get(`https://openexchangerates.org/api/latest.json?app_id=${process.env.EXCHANGE_API_KEY}`);
-    const rate = response.data.rates[target] / response.data.rates[base];
+    const response = await axios.get(`https://api.forexrateapi.com/v1/latest?api_key=${process.env.FOREX_API_KEY}&base=${base}&currencies=${target}`);
+    const rate = response.data.rates[target] 
     res.json({ rate });
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch exchange rate" });
   }
 };
+
+//http://localhost:5000/api/currencyEx?base=LKR&target=USD example url
--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ const authRoutes = require("./routes/authRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
 const budgetRoutes = require("./routes/budgetRoutes");
 
+const currencyController = require("./controllers/currencyController");
+
 dotenv.config();
 connectDB();
 
@@ -20,6 +22,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/budgets", budgetRoutes);
+
+app.use("/api/currencyEx", currencyController.getExchangeRate);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
