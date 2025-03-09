906e38c6f745db246c2e582c162ee2e6412e92a9
Praboth Sandaruwan <bleezbub12@gmail.com>
2025-03-09 10:14:54 +0530

    admin authentication + authorisation cmp no swagger

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -37,3 +37,39 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.registerAdmin = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = new User({ name, email, password: hashedPassword, role: "admin" });
+    await user.save();
+
+    res.status(201).json({ message: "Admin registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email, role: "admin" });
+    if (!user) return res.status(400).json({ message: "Invalid credentials No admins in this email" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { register, login } = require("../controllers/authController");
+const { register, login, loginAdmin, registerAdmin } = require("../controllers/authController");
 
 const router = express.Router();
 
+//user routes
+
 router.post("/register", register);
 router.post("/login", login);
+
+//admin routes
+
+router.post("/register/admin", registerAdmin);
+router.post("/login/admin", loginAdmin);
+
 
 module.exports = router;
