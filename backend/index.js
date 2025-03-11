2a6a62942b521344dbb03a7ede2ed96f3ab1d8ef
Praboth Sandaruwan <bleezbub12@gmail.com>
2025-03-11 05:54:29 +0530

    cmm

--- backend/index.js
+++ backend/index.js
@@ -34,9 +34,5 @@ app.use("/api/transactions", transactionRoutes);
 app.use("/api/budgets", budgetRoutes);
 app.use("/api/goals", goalroutes);
 
-// const PORT = process.env.PORT || 5000;
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 
 module.exports = app;
